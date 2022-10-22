0ec1b2f9a878f9503f6c7357f8873e595cd0d890
promise <promise@DESKTOP-5V7NG46.localdomain>
2022-10-23 00:32:55 +0200

    add terraform config

---
+++ terraform-aws/Database/variables.tf
@@ -0,0 +1,42 @@
+#----database/variables---
+
+variable "db_storage" {
+  
+}
+
+variable "db_instance_class" {
+  
+}
+
+variable "dbname" {
+  
+}
+
+variable "db_engine_version" {
+  
+}
+
+variable "db_identifier" {
+  
+}
+
+
+variable "db_subnet_group_name" {
+  
+}
+
+variable "dbpassword" {
+  
+}
+
+variable "dbuser" {
+  
+}
+
+variable "vpc_security_group_ids" {
+  
+}
+
+variable "skip_final_snapshot" {
+  
+}
---
+++ terraform-aws/loadbalancing/varaibles.tf
@@ -0,0 +1,9 @@
+#----loadbalancing/variables.tf
+
+variable "public_sg" {
+  
+}
+
+variable "public_subnets" {
+  
+}
